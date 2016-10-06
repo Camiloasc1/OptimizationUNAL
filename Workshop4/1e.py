a4c23a6ad6f53adc090d195cc47325ea663c9e98
caasanchezcu <caasanchezcu@unal.edu.co>
2016-10-05 20:37:53 -0500

    Problem #1e.

---
+++ Workshop4/1e.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.optimize import linprog
+
+c = np.array([1, 4, 2])
+A_ub = [
+    [-1, -1, 1],
+    [2, -1, -1]
+]
+b_ub = [-1, 1]
+
+
+def callback(xk, **kwargs):
+    np.set_printoptions(precision=3)
+    np.set_printoptions(suppress=True)
+    print(kwargs)
+    np.savetxt(__file__[:-3] + 'Tableau' + str(kwargs['phase']) + '-' + str(kwargs['nit']) + '.csv', kwargs['tableau'],
+               delimiter=',', fmt='%.3f')
+
+
+res = linprog(c, A_ub=A_ub, b_ub=b_ub, callback=callback)
+print(res)
