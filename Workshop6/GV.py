88d0a6674730feadcaf418145c6f22d1645a5838
caasanchezcu <caasanchezcu@unal.edu.co>
2016-11-12 09:48:22 -0500

    Graphviz printing automated.

--- Workshop6/GV.py
+++ Workshop6/GV.py
@@ -1,4 +1,7 @@
-# dot -Tps G.gv -o G.png
+# dot -Tpng G.gv -o G.png
+import os
+
+file_number = 0
 
 
 def printPrimGraph(G, E):
@@ -17,16 +20,18 @@ def printPrimGraph(G, E):
 
 
 def printEdmonsKarpGraph(C, F):
-    print('digraph {')
-    for u in C:
-        for v in C[u]:
-            R = C[u][v] - F[u][v]
-            if R == 0:
-                print(u, '->', v,
-                      '[label="' + str(F[u][v]) + '/' + str(R) + '/' + str(C[u][v]) + '",weight="' + str(
-                          C[u][v]) + '",color="red"]' + ';')
-            else:
-                print(u, '->', v,
-                      '[label="' + str(F[u][v]) + '/' + str(R) + '/' + str(C[u][v]) + '",weight="' + str(
-                          C[u][v]) + '"]' + ';')
-    print('}')
+    global file_number
+    file_number += 1
+    with open('img/' + str(file_number) + '.gv', 'w') as file:
+        file.write('digraph {' + '\n')
+        for u in C:
+            for v in C[u]:
+                R = C[u][v] - F[u][v]
+                if R == 0 and C[u][v] != 0:
+                    file.write(u + ' -> ' + v + ' [label="' + str(F[u][v]) + '/' + str(R) + '/' + str(
+                        C[u][v]) + '",weight="' + str(C[u][v]) + '",color="red"]' + ';' + '\n')
+                else:
+                    file.write(u + ' -> ' + v + ' [label="' + str(F[u][v]) + '/' + str(R) + '/' + str(
+                        C[u][v]) + '",weight="' + str(C[u][v]) + '"]' + ';' + '\n')
+        file.write('}' + '\n')
+    os.system('dot -Tpng img/' + str(file_number) + '.gv -o img/' + str(file_number) + '.png')
